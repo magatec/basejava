d92905c70afd4a0b77e3e3863131e3024f088d64
magatec <magatec_edu@mail.ru>
2022-03-26 12:01:45 +0200

    Homework, lesson_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,47 @@
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int count = 0;
 
-    void clear() {
+    void clear() {  //удаляем все значения в массиве, обнуляем счетчик
+        for (int i = 0; i < count; i++) {
+            storage[i] = null;
+        }
+        count = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume r) {  //добавляем резюме в массив, увеличиваем счетчик на 1.
+        storage[count] = r;
+        count++;
     }
 
-    Resume get(String uuid) {
-        return null;
+    Resume get(String uuid) {  //возвращаем резюме по его uuid
+        return storage[Integer.parseInt(uuid)];
     }
 
-    void delete(String uuid) {
+    void delete(String uuid) {   //удаляем элемент массива, сдвигаем все элементы стоящие после него на -1, уменьшаем счетчик на 1
+        int id = Integer.parseInt(uuid);
+        storage[id] = null;
+        for (int i = id; i < count; i++) {
+            storage[i] = storage[i + 1];
+        }
+        count--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    Resume[] getAll() {  // возвращааем массив резюме без null
+        Resume[] resumes = new Resume[count];
+        for (int i = 0; i < count; i++)
+            resumes[i] = storage[i];
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return count;
     }
 }
